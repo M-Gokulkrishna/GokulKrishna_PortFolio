6a4fc1a5f3a75df69ceb5c833bb3887be8b13afb
GOKUL KRISHNA M <mgkgokulkrishna78@gmail.com>
2024-07-01 11:16:39 +0530

    Chore: UI Update2

--- portfolio-client/src/Pages/AboutSection.jsx
+++ portfolio-client/src/Pages/AboutSection.jsx
@@ -1,15 +1,28 @@
 import AOS from 'aos';
 import '../Stylesheets/AboutPage.css';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 
 const AboutSection = () => {
+    const AboutContentBox1Ref = useRef(null);
+    const AboutContentBox2Ref = useRef(null);
     const [LableClick, setLabelClick] = useState(false);
+    const [AboutContentBox1Height, setAboutContentBox1Height] = useState('');
+    const [AboutContentBox2Height, setAboutContentBox2Height] = useState('');
 
     useEffect(() => {
         AOS.init({
             duration: 900
         });
     }, [])
+
+    useEffect(() => {
+        setAboutContentBox1Height(AboutContentBox1Ref.current.offsetHeight);
+        setAboutContentBox2Height(AboutContentBox2Ref.current.offsetHeight);
+        window.addEventListener('resize', () => {
+            setAboutContentBox1Height(AboutContentBox1Ref.current.offsetHeight);
+            setAboutContentBox2Height(AboutContentBox2Ref.current.offsetHeight);
+        });
+    }, [AboutContentBox1Height, AboutContentBox2Height])
 
     return (
         <div id='About-Page'>
@@ -21,11 +34,11 @@ const AboutSection = () => {
                     <h3 className={`Description ${(!LableClick) ? 'active' : ''} position-relative py-2`} onClick={() => setLabelClick(false)}>Description</h3>
                     <h3 className={`Education ${(LableClick) ? 'active' : ''} position-relative py-2`} onClick={() => setLabelClick(true)}>Education</h3>
                 </div>
-                <div className="Contents-Container">
-                    <div className='About-Content1' style={(!LableClick) ? { transform: "translateY(0%)" } : { transform: "translateY(180%)" }}>
+                <div className="Contents-Container" style={(AboutContentBox1Height>AboutContentBox2Height)?{minHeight: `${AboutContentBox1Height}px`}:{minHeight: `${AboutContentBox2Height}px`}}>
+                    <div className='About-Content1' ref={AboutContentBox1Ref} style={(!LableClick) ? { transform: "translateY(0%)" } : { transform: "translateY(180%)" }}>
                         <h5>Passionate front end developer have expertise in React js. Specialize in building and designing a Responsive and visually appealing Websites. I have competence in various technologies such as HTML, CSS, JavaScript, Python, React.js, Git, MySQL, Mongo DB, Node.js & Express.js. <hr /> I'm a Enthusiastic individual to learn new technologies and enhancing My skiils. I ensure that I will provide a successful outcomes in my work and contribute to the success of the Organization.</h5>
                     </div>
-                    <div className='About-Content2' style={(LableClick) ? { transform: "translateY(0%)" } : { transform: "translateY(180%)" }}>
+                    <div className='About-Content2' ref={AboutContentBox2Ref} style={(LableClick) ? { transform: "translateY(0%)" } : { transform: "translateY(180%)" }}>
                         <div>
                             <h3>Full Stack Development</h3>
                             <h4>NxtWave</h4>
